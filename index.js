069719f1e8346ad22756e1c1b1585d0fb5d108ba
Bsher <brahwan@red.sa>
2022-06-10 14:17:20 -0400

    change

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.static(path.join(__dirname,'build')));
 app.use(express.static(path.join(__dirname,'staticPages')));
 
 app.get('/login', (req, res) => {
+    console.log("tree: ",_getAllFilesFromFolder(__dirname))
     res.sendFile(path.join(__dirname, './staticPages/loginPage/index.html'));
 });
 
@@ -18,5 +19,25 @@ app.get('/*', isLoggedIn, (req, res) => {
 app.listen(port, ()=>{
     console.log(`Server now listening at htttp://localhost:${port}`);
 })
+
+var _getAllFilesFromFolder = function(dir) {
+
+    var filesystem = require("fs");
+    var results = [];
+
+    filesystem.readdirSync(dir).forEach(function(file) {
+
+        file = dir+'/'+file;
+        var stat = filesystem.statSync(file);
+
+        if (stat && stat.isDirectory()) {
+            results = results.concat(_getAllFilesFromFolder(file))
+        } else results.push(file);
+
+    });
+
+    return results;
+
+};
 
 module.exports = app;
