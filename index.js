d471e451781ceb7e7b4513cd033377d7d373bf7c
Bsher <brahwan@red.sa>
2022-06-10 08:46:38 -0400

    step5

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const port = 3100;
 const path = require('path');
 const isLoggedIn = require('./isLoggedIn')
 
-app.use(express.static(path.join(__dirname,'..','build')));
-app.use(express.static(path.join(__dirname,'..','staticPages')));
+app.use(express.static(path.join(__dirname,'build')));
+app.use(express.static(path.join(__dirname,'staticPages')));
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '../staticPages/loginPage/index.html'));
+    res.sendFile(path.join(__dirname, './staticPages/loginPage/index.html'));
 });
 
 app.get('/*', isLoggedIn, (req, res) => {
-    res.sendFile(path.join(__dirname, '../build/index.html'));
+    res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
 app.listen(port, ()=>{
