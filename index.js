c08c0655d17772d8bdb1e119a4e450999e9fe545
Bsher <brahwan@red.sa>
2022-06-10 15:26:50 -0400

    a change

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.static(path.join(__dirname,'staticPages')));
 
 app.get('/dashboard', isLoggedIn, (req, res) => {
     res.sendFile(path.join(__dirname, './build/index.html'));
+    
 });
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, './staticPages/loginPage/index.html'));
