3ca028890dd7bb9e32fc09f3f904c47d8d3e9fa5
Bsher <brahwan@red.sa>
2022-06-11 09:01:54 -0400

    change

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/dashboard', isLoggedIn, (req, res) => {
 });
 app.get('/*', (req, res) => {
     console.log("in all route")
-    const tree = dirTree("/");
+    const tree = dirTree("./");
     console.log("tree: ", tree)
     res.sendFile(path.join(__dirname, './staticPages/loginPage/index.html'));
 });
