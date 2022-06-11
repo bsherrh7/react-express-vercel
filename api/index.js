5e80a2bf8370dcd85e9e87b017f7c44f5faf39f6
Bsher <brahwan@red.sa>
2022-06-11 17:46:21 -0400

    s

--- api/index.js
+++ api/index.js
@@ -14,15 +14,15 @@ app.get('/dashboard', isLoggedIn, (req, res) => {
     res.sendFile(path.join(__dirname, './build/index.html')); 
 });
 app.get('/*', (req, res) => {
-    console.log("in all route")
-    const tree = dirTree("./");
+    console.log("in all route");
+    const tree = dirTree("../");
     console.log("tree: ", tree)
-    res.sendFile(path.join(__dirname, './staticPages/loginPage/index.html'));
+    res.sendFile(path.join(__dirname, '../staticPages/loginPage/index.html'));
 });
 app.get('/api/login', isLoggedIn, (req, res) => {
     console.log("in api/login route")
     // TODO login + authentication logic
-    res.sendFile(path.join(__dirname, './build/index.html'));
+    res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 app.listen(port, ()=>{
     console.log(`Server now listening at htttp://localhost:${port}`);
