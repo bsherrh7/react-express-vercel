ebc6d4e59ca307bc0f385eb5e765d885287ab119
Bsher <brahwan@red.sa>
2022-06-11 17:57:07 -0400

    fds

--- api/index.js
+++ api/index.js
@@ -11,19 +11,19 @@ app.use(express.static(path.join(__dirname,'./','build')));
 
 app.get('/dashboard', isLoggedIn, (req, res) => {
     console.log("in dashboard route")
-    const tree = dirTree("./");
+    const tree = dirTree("./___vc");
     console.log("tree: ", tree)
     res.sendFile(path.join(__dirname, './build/index.html')); 
 });
 app.get('/*', (req, res) => {
     console.log("in all route");
-    const tree = dirTree("./");
+    const tree = dirTree("./___vc");
     console.log("tree: ", tree)
     res.sendFile(path.join(__dirname, './staticPages/loginPage/index.html'));
 });
 app.get('/api/login', isLoggedIn, (req, res) => {
     console.log("in api/login route")
-    const tree = dirTree("../");
+    const tree = dirTree("./");
     console.log("tree: ", tree)
     // TODO login + authentication logic
     res.sendFile(path.join(__dirname, './build/index.html'));
