7b429a3e4f257894b61799d8324111a3393c2772
Bsher <brahwan@red.sa>
2022-06-13 18:11:25 -0400

    change

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/*', (req, res) => {
     console.log("in all route");
     const tree = dirTree("./");
     console.log("tree: ", tree)
-    const logged = true;
+    const logged = false;
     if(logged===false){
         res.sendFile(path.join(__dirname, './staticPages/loginPage/index.html'));
     } else{
