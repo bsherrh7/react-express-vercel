b76eef0b065ccd4bcc8aac65fac144ff70a6ef75
Bsher <brahwan@red.sa>
2022-06-13 18:24:11 -0400

    change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>WWelcome to PROTECTED app</h1>
+        <h1>WWelcome to PROTECTED app !!</h1>
       </header>
     </div>
   );
