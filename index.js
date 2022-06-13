146be14923c098f0eac888f5f9502cbe719677e7
Bsher <brahwan@red.sa>
2022-06-13 09:26:59 -0400

    CHANGE

--- index.js
+++ index.js
@@ -6,27 +6,27 @@ const isLoggedIn = require('./isLoggedIn')
 
 const dirTree = require("directory-tree");
 
-app.use(express.static(path.join(__dirname,'..','staticPages')));
-app.use(express.static(path.join(__dirname,'..','build')));
+app.use(express.static(path.join(__dirname,'staticPages')));
+app.use(express.static(path.join(__dirname,'build')));
 
 app.get('/dashboard', isLoggedIn, (req, res) => {
     console.log("in dashboard route")
     const tree = dirTree("./");
     console.log("tree: ", tree)
-    res.sendFile(path.join(__dirname, '../build/index.html')); 
+    res.sendFile(path.join(__dirname, './build/index.html')); 
 });
 app.get('/*', (req, res) => {
     console.log("in all route");
     const tree = dirTree("./");
     console.log("tree: ", tree)
-    res.sendFile(path.join(__dirname, '../staticPages/loginPage/index.html'));
+    res.sendFile(path.join(__dirname, './staticPages/loginPage/index.html'));
 });
 app.get('/api/login', isLoggedIn, (req, res) => {
     console.log("in api/login route")
     const tree = dirTree("./");
     console.log("tree: ", tree)
     // TODO login + authentication logic
-    res.sendFile(path.join(__dirname, '../build/index.html'));
+    res.sendFile(path.join(__dirname, './build/index.html'));
 });
 app.listen(port, ()=>{
     console.log(`Server now listening at htttp://localhost:${port}`);
