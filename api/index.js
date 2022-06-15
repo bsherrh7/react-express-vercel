153d1474dcd601cf8d894143d740934bba56a4a4
Bsher <brahwan@red.sa>
2022-06-15 12:42:28 -0400

    change

--- api/index.js
+++ api/index.js
@@ -16,40 +16,6 @@ app.use(express.static(path.join(__dirname,'..','build')));
 
 
 
-
-const App = require("../build/App.js")
-const React = require("react")
-const renderToString = require("react-dom/server");
-const hbs = require("handlebars");
-
-
-
-app.get("/1", async (req, res) => {
-  const theHtml = `
-  <html>
-  <head><title>My First SSR</title></head>
-  <body>
-  <h1>My First Server Side Render</h1>
-  <div id="reactele">{{{reactele}}}</div>
-  <script src="/app.js" charset="utf-8"></script>
-  <script src="/vendor.js" charset="utf-8"></script>
-  </body>
-  </html>
-  `;
-
-  const hbsTemplate = hbs.compile(theHtml);
-  const reactComp = renderToString(<App />);
-  const htmlToSend = hbsTemplate({ reactele: reactComp });
-  res.send(htmlToSend);
-});
-
-
-
-
-
-
-
-
 const waitSomeTime =(watiTimeMillisec)=>{
     return new Promise((resolve) => {
         setTimeout(() => resolve(true), watiTimeMillisec);
