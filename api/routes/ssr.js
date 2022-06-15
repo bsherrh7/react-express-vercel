780c3c17bc002d0a0908a3d49f621e8f0a5c0286
Bsher <brahwan@red.sa>
2022-06-15 12:29:49 -0400

    change

--- api/routes/ssr.js
+++ api/routes/ssr.js
@@ -1,6 +1,19 @@
 import express from "express";
 const router = express.Router();
+
 router.get("/", async (req, res) => {
-res.status(201).send("Hello World");
-});
+    const theHtml = `
+    <html>
+    <head><title>My First SSR</title></head>
+    <body>
+    <h1>My First Server Side Render</h1>
+    <div id="reactele">{{{reactele}}}</div>
+    <script src="/app.js" charset="utf-8"></script>
+    <script src="/vendor.js" charset="utf-8"></script>
+    </body>
+    </html>
+    `;
+    res.send(theHtml);
+    });
+
 export default router;
