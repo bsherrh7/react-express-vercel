6c9b27a95a85df57b9e0a1df3a3d7a2e52f358cf
Bsher <brahwan@red.sa>
2022-06-16 08:57:19 -0400

    changes

---
+++ server/auth.js
@@ -0,0 +1,45 @@
+import React from "react";
+import express from "express";
+import * as ReactDOMServer from 'react-dom/server';
+// import  App  from "./App";
+const path = require('path');
+const dirTree = require("directory-tree");
+const app = express();
+
+app.use(express.static(path.join(__dirname,'..','static/pages/login')));
+app.use(express.static(path.join(__dirname,'..','build')));
+
+
+app.get('*',(req,res)=>{
+    const tree = dirTree("./");
+    console.log("in auth !!");
+    res.sendFile(path.join(__dirname, '../static/pages/login/index.html'));
+})
+
+app.get('/1', async (req, res) => {
+
+    // const app = ReactDOMServer.renderToString(<App />);
+    // const html = `
+    //     <html lang="en">
+    //     <head>
+    //         <link rel="stylesheet" href="app.css">
+    //         <script src="app.js" async defer></script>
+    //     </head>
+    //     <body>
+    //         <div id="root">${app}</div>
+    //     </body>
+    //     </html>
+    // `
+    // const waitSomeTime =(watiTimeMillisec)=>{
+    //     return new Promise((resolve) => {
+    //         setTimeout(() => resolve(true), watiTimeMillisec);
+    //     });
+    // }
+    // await waitSomeTime(4000)
+    res.send("html");
+});
+
+
+app.listen(4000,()=>{
+    console.log("running on 4000!")
+});
