6a0957f2f37340783eaa13ec57bdb57a84549351
Bsher <brahwan@red.sa>
2022-06-16 12:19:07 -0400

    change

--- server/auth.js
+++ server/auth.js
@@ -1,7 +1,7 @@
 import React from "react";
 import express from "express";
 import * as ReactDOMServer from 'react-dom/server';
-// import  App  from "./static/App";
+import  App  from "./App";
 const path = require('path');
 const dirTree = require("directory-tree");
 
