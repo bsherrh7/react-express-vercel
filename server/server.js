40933d907db64c35ece6ccfc1d1cab69513a1b3d
Bsher <brahwan@red.sa>
2022-06-16 09:13:18 -0400

    change

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const auth = require("../build/auth")
 const app = express();
 const path = require('path');
 
+const dirTree = require("directory-tree");
+const tree = dirTree("./");
+console.log("tree: ",tree);
+console.log("auth: ",auth)
+
 app.use(express.static(path.join(__dirname,'..','static/pages/login')));
 app.use(express.static(path.join(__dirname,'..','build')));
 
