56bf295479408fe1e4eb996b58f63304b668da9e
Bsher <brahwan@red.sa>
2022-06-16 12:10:45 -0400

    change

--- server/server.js
+++ server/server.js
@@ -8,13 +8,15 @@ const path = require('path');
 // console.log("tree: ",tree);
 // console.log("auth: ",auth)
 
+
+app.use(express.static(path.join(__dirname,'..','static/pages/login')));
+app.use(express.static(path.join(__dirname,'..','build')));
+
+
 app.use("/auth",auth);
 app.use("/*",(req, res)=>{
     console.log("in all routes");
     res.send("path.join(__dirname, '../static/pages/login/index.html')");
 })
-
-app.use(express.static(path.join(__dirname,'..','static/pages/login')));
-app.use(express.static(path.join(__dirname,'..','build')));
 
 module.exports = app;
