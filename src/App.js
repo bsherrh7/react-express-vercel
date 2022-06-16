a0b31a4d39a03b11a57a9c9722f392785cc8069a
Bsher <brahwan@red.sa>
2022-06-16 15:15:44 -0400

    change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React App!</h1>
+        <h1>React Protected App!</h1>
       </header>
     </div>
   );
