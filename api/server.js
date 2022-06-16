67affe185863db438078f7201574beeba540b810
Bsher <brahwan@red.sa>
2022-06-16 08:09:24 -0400

    change

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 import React from "react";
 import express from "express";
 import * as ReactDOMServer from 'react-dom/server';
-import  App  from "./App";
+// import  App  from "./App";
 const path = require('path');
 
 const app = express();
@@ -50,25 +50,25 @@ app.get('/login',(req,res)=>{
 
 app.get('/1', async (req, res) => {
 
-    const app = ReactDOMServer.renderToString(<App />);
-    const html = `
-        <html lang="en">
-        <head>
-            <link rel="stylesheet" href="app.css">
-            <script src="app.js" async defer></script>
-        </head>
-        <body>
-            <div id="root">${app}</div>
-        </body>
-        </html>
-    `
-    const waitSomeTime =(watiTimeMillisec)=>{
-        return new Promise((resolve) => {
-            setTimeout(() => resolve(true), watiTimeMillisec);
-        });
-    }
-    await waitSomeTime(4000)
-    res.send(html);
+    // const app = ReactDOMServer.renderToString(<App />);
+    // const html = `
+    //     <html lang="en">
+    //     <head>
+    //         <link rel="stylesheet" href="app.css">
+    //         <script src="app.js" async defer></script>
+    //     </head>
+    //     <body>
+    //         <div id="root">${app}</div>
+    //     </body>
+    //     </html>
+    // `
+    // const waitSomeTime =(watiTimeMillisec)=>{
+    //     return new Promise((resolve) => {
+    //         setTimeout(() => resolve(true), watiTimeMillisec);
+    //     });
+    // }
+    // await waitSomeTime(4000)
+    res.send("html");
 });
 
 app.use(express.static("./build"));
