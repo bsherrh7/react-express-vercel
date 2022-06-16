a544be770993918d094e1dfee7abb13415122a78
Bsher <brahwan@red.sa>
2022-06-16 12:18:25 -0400

    change

--- server/auth.js
+++ server/auth.js
@@ -1,9 +1,13 @@
 import React from "react";
 import express from "express";
 import * as ReactDOMServer from 'react-dom/server';
-import  App  from "./static/App";
+// import  App  from "./static/App";
 const path = require('path');
 const dirTree = require("directory-tree");
+
+const tree = dirTree("./");
+console.log("treeeee: ",tree);
+
 
     const waitSomeTime =(watiTimeMillisec)=>{
         return new Promise((resolve) => {
