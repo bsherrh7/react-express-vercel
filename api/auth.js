67769793ec7d83368fc484f855ba308f555b458a
Bsher <brahwan@red.sa>
2022-06-16 12:44:49 -0400

    change

--- api/auth.js
+++ api/auth.js
@@ -1,8 +1,6 @@
 import React from "react";
-import express from "express";
 import * as ReactDOMServer from 'react-dom/server';
-import  App  from "./AppComponent/App";
-const path = require('path');
+import App from "./AppComponent/App";
 const dirTree = require("directory-tree");
 
 const tree = dirTree("./");
@@ -36,27 +34,4 @@ const auth = async (req,res)=>{
     await waitSomeTime(4000)
     res.send(html);
  }
-
-// router.get('/1', async (req, res) => {
-
-//     // const app = ReactDOMServer.renderToString(<App />);
-//     // const html = `
-//     //     <html lang="en">
-//     //     <head>
-//     //         <link rel="stylesheet" href="app.css">
-//     //         <script src="app.js" async defer></script>
-//     //     </head>
-//     //     <body>
-//     //         <div id="root">${app}</div>
-//     //     </body>
-//     //     </html>
-//     // `
-//     // const waitSomeTime =(watiTimeMillisec)=>{
-//     //     return new Promise((resolve) => {
-//     //         setTimeout(() => resolve(true), watiTimeMillisec);
-//     //     });
-//     // }
-//     // await waitSomeTime(4000)
-//     res.send("html");
-// });
 module.exports = auth
