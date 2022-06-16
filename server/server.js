f0c676cf0502be1b9078ce82e728cc33b38ec618
Bsher <brahwan@red.sa>
2022-06-16 08:44:23 -0400

    change

--- server/server.js
+++ server/server.js
@@ -10,49 +10,9 @@ app.use(express.static(path.join(__dirname,'..','static/pages/login')));
 app.use(express.static(path.join(__dirname,'..','build')));
 
 
-
-app.get("/login",(req,res)=>{
-    const tree = dirTree("./");
-console.log("tree: ", tree)
-  const html =`
-  <html>
-    <head>
-        <link href="https://fonts.cdnfonts.com/css/product-sans" rel="stylesheet">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.24.0/axios.min.js" integrity="sha512-u9akINsQsAkG9xjc1cnGF4zw5TFDwkxuc9vUp5dltDWYCSmyd0meygbvgXrlc/z7/o4a19Fb5V0OUE58J7dcyw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <link rel="stylesheet" href="login-page.css">
-    </head>
-    <title>Basetwoo</title>
-    <body>
-        <div id="container">
-            <span id="loading-text">Loading</span>
-            <div class="lds-dual-ring"></div>
-        </div>
-        <script>
-            const LOCAL_STORAGE_KEYS ={
-                ID_TOKEN: "ID_TOKEN",
-            }
-            const isClientAuth = () => {
-                let id_token = localStorage.getItem(LOCAL_STORAGE_KEYS.ID_TOKEN)
-                const url = "/1";
-                console.log("url: ",url);
-                axios.get(url)
-                .then(res => {
-                    if(res.status===200){
-                        console.log("page", res.data)
-                        document.write(res.data);
-
-                    }
-                })
-                .catch(error => console.error(error));
-            };
-            isClientAuth();
-        </script>
-    </body>
-</html>`
-res.send(html)
-})
-
 app.get('/login',(req,res)=>{
+    const tree = dirTree("./");
+    console.log("tree: ", tree)
     res.sendFile(path.join(__dirname, '../static/pages/login/index.html'));
 })
 
