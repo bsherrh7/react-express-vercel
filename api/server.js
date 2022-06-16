32bab0fd135988b33f94800d7c36dc342df0219e
Bsher <brahwan@red.sa>
2022-06-16 07:47:29 -0400

    change

--- api/server.js
+++ api/server.js
@@ -1,4 +1,13 @@
-<html>
+import React from "react";
+import express from "express";
+import * as ReactDOMServer from 'react-dom/server';
+import  App  from "./App";
+const path = require('path');
+
+const app = express();
+app.get("/login",(req,res)=>{
+  const html =`
+  <html>
     <head>
         <link href="https://fonts.cdnfonts.com/css/product-sans" rel="stylesheet">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.24.0/axios.min.js" integrity="sha512-u9akINsQsAkG9xjc1cnGF4zw5TFDwkxuc9vUp5dltDWYCSmyd0meygbvgXrlc/z7/o4a19Fb5V0OUE58J7dcyw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -16,7 +25,7 @@
             }
             const isClientAuth = () => {
                 let id_token = localStorage.getItem(LOCAL_STORAGE_KEYS.ID_TOKEN)
-                const url = "/api/isClientAuth";
+                const url = "/1";
                 console.log("url: ",url);
                 axios.get(url)
                 .then(res => {
@@ -28,7 +37,43 @@
                 })
                 .catch(error => console.error(error));
             };
-            // isClientAuth();
+            isClientAuth();
         </script>
     </body>
-</html>
+</html>`
+res.send(html)
+})
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname, '../static/pages/login/index.html'));
+})
+
+app.get('/1', async (req, res) => {
+
+    const app = ReactDOMServer.renderToString(<App />);
+    const html = `
+        <html lang="en">
+        <head>
+            <link rel="stylesheet" href="app.css">
+            <script src="app.js" async defer></script>
+        </head>
+        <body>
+            <div id="root">${app}</div>
+        </body>
+        </html>
+    `
+    const waitSomeTime =(watiTimeMillisec)=>{
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(true), watiTimeMillisec);
+        });
+    }
+    await waitSomeTime(4000)
+    res.send(html);
+});
+
+app.use(express.static("./build"));
+app.use(express.static('../static/pages/login'));
+
+app.listen(4000,()=>{
+    console.log("running on 4000!")
+});
