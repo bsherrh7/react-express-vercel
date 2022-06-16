ff71970285b29bbe81af27c8deba59f6e523c1af
Bsher <brahwan@red.sa>
2022-06-16 12:01:21 -0400

    change

--- server/auth.js
+++ server/auth.js
@@ -1,15 +1,22 @@
 import React from "react";
 import express from "express";
 import * as ReactDOMServer from 'react-dom/server';
-// import  App  from "./App";
+import  App  from "./App";
 const path = require('path');
 const dirTree = require("directory-tree");
 
-
-const auth = (req,res)=>{
+    const waitSomeTime =(watiTimeMillisec)=>{
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(true), watiTimeMillisec);
+        });
+    }
+    
+const auth = async (req,res)=>{
     const tree = dirTree("./");
     console.log("tree: ",tree);
     console.log("in auth !!");
+    console.log("app: ",app)
+    await waitSomeTime(4000)
     res.sendFile(path.join(__dirname, '../static/pages/login/index.html'));
 // })
 
